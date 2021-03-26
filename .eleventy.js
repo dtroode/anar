7c4443581168c9a4fd575fa9d5f959863b116886
David Kistauri <davidkis113@gmail.com>
2021-03-26 12:12:58 +0300

    Add manifest and icons rules

--- .eleventy.js
+++ .eleventy.js
@@ -31,6 +31,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/static/fonts");
   eleventyConfig.addPassthroughCopy("./src/favicon.ico");
   eleventyConfig.addPassthroughCopy("./src/robots.txt");
+  eleventyConfig.addPassthroughCopy("./src/manifest.webmanifest");
 
   // Add partners to collection
   eleventyConfig.addCollection(
